a1e07778995f288c4d5430e8036457472167d31e
Moritz Finke <abzicht@gmail.com>
2024-07-08 10:30:57 +0200

    moved either flat map

--- fun/either.go
+++ fun/either.go
@@ -3,10 +3,10 @@ package fun
 /*
 Either holds either a value of type A or of type E.
 */
-type Either[A, E any] interface {
+type Either[E, A any] interface {
 	GetLeft() E
 	GetRight() A
-	FlatMap(func(A) Either[A, E]) Either[A, E]
+	//FlatMap(func(A) Either[E, A]) Either[E, A]
 }
 
 type EitherError struct {
@@ -17,28 +17,52 @@ func (ee EitherError) Error() string {
 	return ee.Msg
 }
 
-type Left[A, E any] struct {
+type Left[E, A any] struct {
 	E E
 }
 
-func NewLeft[A, E any](e E) Either[A, E] {
-	return Left[A, E]{e}
+func NewLeft[E, A any](e E) Either[E, A] {
+	return Left[E, A]{e}
 }
 
-func (l Left[A, E]) GetLeft() E                                  { return l.E }
-func (l Left[A, E]) GetRight() A                                 { panic(EitherError{"Requested right value from a left object"}) }
-func (l Left[A, E]) FlatMap(f func(A) Either[A, E]) Either[A, E] { return l }
+func (l Left[E, A]) GetLeft() E  { return l.E }
+func (l Left[E, A]) GetRight() A { panic(EitherError{"Requested right value from a left object"}) }
 
-type Right[A, E any] struct {
+type Right[E, A any] struct {
 	A A
 }
 
-func NewRight[A, E any](a A) Either[A, E] {
-	return Right[A, E]{a}
+func NewRight[E, A any](a A) Either[E, A] {
+	return Right[E, A]{a}
 }
 
-func (r Right[A, E]) GetLeft() E  { panic(EitherError{"Requested left value from a right object"}) }
-func (r Right[A, E]) GetRight() A { return r.A }
-func (r Right[A, E]) FlatMap(f func(A) Either[A, E]) Either[A, E] {
-	return f(r.GetRight())
+func (r Right[E, A]) GetLeft() E  { panic(EitherError{"Requested left value from a right object"}) }
+func (r Right[E, A]) GetRight() A { return r.A }
+
+func EitherFlatMap[E, A, B any](either Either[E, A], mapFunc func(A) Either[E, B]) Either[E, B] {
+	switch either.(type) {
+	case Right[E, A]:
+		return mapFunc(either.GetRight())
+	case Left[E, A]:
+		return NewLeft[E, B](either.GetLeft())
+	}
+	panic(EitherError{"Provided either is neither left nor right"})
+}
+
+func EitherMap[E, A, B any](either Either[E, A], mapFunc func(A) B) Either[E, B] {
+	switch either.(type) {
+	case Right[E, A]:
+		return NewRight[E, B](mapFunc(either.GetRight()))
+	case Left[E, A]:
+		return NewLeft[E, B](either.GetLeft())
+	}
+	panic(EitherError{"Provided either is neither left nor right"})
+}
+
+func EitherTry[A any](tryFunc func() (A, error)) Either[error, A] {
+	resultA, resultE := tryFunc()
+	if resultE != nil {
+		return NewLeft[error, A](resultE)
+	}
+	return NewRight[error, A](resultA)
 }
--- fun/either_test.go
+++ fun/either_test.go
@@ -11,14 +11,14 @@ func TestLeft(t *testing.T) {
 	type A int
 	type E error
 
-	var l Either[A, E]
+	var l Either[E, A]
 
 	testErr := EitherError{"left error"}
-	l = NewLeft[A, E](testErr)
+	l = NewLeft[E, A](testErr)
 	assert.Equal(t, testErr, l.GetLeft(), "Error in either is not correctly returned")
 	assert.Panics(t, func() { _ = l.GetRight() }, "Expected left to panic at request for right")
-	assert.True(t, cmp.Equal(l, NewLeft[A, E](EitherError{"left error"})))
-	assert.False(t, cmp.Equal(l, NewRight[A, E](0)))
+	assert.True(t, cmp.Equal(l, NewLeft[E, A](EitherError{"left error"})))
+	assert.False(t, cmp.Equal(l, NewRight[E, A](0)))
 }
 
 func TestRight(t *testing.T) {
@@ -28,36 +28,36 @@ func TestRight(t *testing.T) {
 	}
 	type E error
 
-	var r Either[A, E]
+	var r Either[E, A]
 
 	a := A{5, "test string"}
 
-	r = NewRight[A, E](a)
+	r = NewRight[E, A](a)
 	assert.Equal(t, a, r.GetRight(), "Value in either is not correctly returned")
 	assert.Panics(t, func() { _ = r.GetLeft() }, "Expected right to panic at request for left")
-	assert.True(t, cmp.Equal(r, NewRight[A, E](A{5, "test string"})))
-	assert.False(t, cmp.Equal(r, NewLeft[A, E](EitherError{"an error"})))
+	assert.True(t, cmp.Equal(r, NewRight[E, A](A{5, "test string"})))
+	assert.False(t, cmp.Equal(r, NewLeft[E, A](EitherError{"an error"})))
 
-	flattenedR := r.FlatMap(func(a A) Either[A, E] {
+	flattenedR := EitherFlatMap[E, A, A](r, func(a A) Either[E, A] {
 		if a.A < 6 {
-			return NewLeft[A, E](EitherError{"smaller than 6"})
+			return NewLeft[E, A](EitherError{"smaller than 6"})
 		}
 		return r
 	})
 	switch flattenedR.(type) {
-	case Left[A, E]:
+	case Left[E, A]:
 		break
 	default:
 		t.Error("Expected Left")
 	}
-	flattenedR = r.FlatMap(func(a A) Either[A, E] {
+	flattenedR = EitherFlatMap[E, A, A](r, func(a A) Either[E, A] {
 		if a.A < 5 {
-			return NewLeft[A, E](EitherError{"smaller than 5"})
+			return NewLeft[E, A](EitherError{"smaller than 5"})
 		}
 		return r
 	})
 	switch flattenedR.(type) {
-	case Right[A, E]:
+	case Right[E, A]:
 		break
 	default:
 		t.Error("Expected Right")
